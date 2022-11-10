27daffffb9c88f1fe534afcf6c4c4303d7b5e398
maronuu <oguri.ytr.2000@gmail.com>
2022-11-11 01:35:42 +0900

    Add remove button to erase completed tasks

--- pages/todo/index.tsx
+++ pages/todo/index.tsx
@@ -49,11 +49,8 @@ const TodoListPage = () => {
   }
 
   function sweepDoneTasks() {
-    taskList.forEach((element, index) => {
-      if (element.status == "done") {
-        taskList.splice(index, 1);
-      }
-    })
+    let newTaskList = taskList.filter(task => task.status == "wip")
+    setTaskList(newTaskList)
   }
 
   return (
@@ -81,7 +78,7 @@ const TodoListPage = () => {
             }}
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
-            Register Task
+            Register New Task
           </button>
           <button
             onClick={sweepDoneTasks}
